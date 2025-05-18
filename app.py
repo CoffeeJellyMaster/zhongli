6eb5fff09ae4838041bb25301fae68bc0e5ff6bf
CoffeeJellyMaster <23-03014@g.batstate-u.edu.ph>
2025-05-18 23:09:10 +0800

    supabas integration

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # loads variables from .env into environment
+
+SUPABASE_URL = os.getenv('SUPABASE_URL')
+SUPABASE_KEY = os.getenv('SUPABASE_KEY')
 
 app = Flask(__name__)
 
